2be01a55d8e1fd898b09af6b69aa4a35a244abde
oscarbasto5627 <47487344+oscarbasto5627@users.noreply.github.com>
2022-04-28 12:12:28 -0500

    Create Homework.js

---
+++ Homework.js
@@ -0,0 +1,9 @@
+ 
+var casa = 5;
+var perro = 2; 
+
+
+return "la multiplicacion de :" casa * perro;
+return "la suma de :" casa + perro;
+return "la division de :" casa / perro;
+reutnr "la resta de :" casa - perro;
